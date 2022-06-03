8f0aad99a9248074314a4d836c249173f699774e
Oliver Haagh <oliver@dmc.international>
2022-06-03 10:05:54 +0200

    Update copyright header

--- src/main/java/com/example/mrr/model/NamespaceSyntax.java
+++ src/main/java/com/example/mrr/model/NamespaceSyntax.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Digital Maritime Consultancy
+ * Copyright 2022 International Association of Marine Aids to Navigation and Lighthouse Authorities
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
