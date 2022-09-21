8bf98babe44fee8025ff31376d7ed0618d656937
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2022-09-21 17:58:04 +0200

    Allow all OPTIONS calls to make CORS work

--- src/main/java/org/iala_aism/mrr/security/SecurityConfig.java
+++ src/main/java/org/iala_aism/mrr/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authz -> authz
+                        .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .mvcMatchers(HttpMethod.GET, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
