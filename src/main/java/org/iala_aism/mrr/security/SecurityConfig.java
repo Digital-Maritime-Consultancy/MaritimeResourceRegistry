196e918fe3838b5d61928ac86fa904907284aeb4
Oliver Haagh <oliver@dmc.international>
2022-09-28 11:02:32 +0200

    Register SimpleCorsFilter

--- src/main/java/org/iala_aism/mrr/security/SecurityConfig.java
+++ src/main/java/org/iala_aism/mrr/security/SecurityConfig.java
@@ -16,19 +16,30 @@
 
 package org.iala_aism.mrr.security;
 
+import org.iala_aism.mrr.config.SimpleCorsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private SimpleCorsFilter simpleCorsFilter;
+
+    @Autowired
+    public void setSimpleCorsFilter(SimpleCorsFilter simpleCorsFilter) {
+        this.simpleCorsFilter = simpleCorsFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(simpleCorsFilter, ChannelProcessingFilter.class)
                 .authorizeRequests(authz -> authz
                         .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .mvcMatchers(HttpMethod.GET, "/**").permitAll()
